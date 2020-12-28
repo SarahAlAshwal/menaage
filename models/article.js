e24f88cdfa3cd03bc2eb9b904d1ead8ba783df6e
Silwan Aleryani <silwanaleryani@Silwans-iMac.local>
2020-12-27 23:18:39 -0500

    finish the articles

--- models/article.js
+++ models/article.js
@@ -1,5 +1,9 @@
-const mongoose = require ('mongoose');
-const slugify = require('slugify');
+const mongoose = require('mongoose')
+const marked = require('marked')
+const slugify = require('slugify')
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)
 
 const articleSchema = new mongoose.Schema({
   title: {
@@ -9,25 +13,35 @@ const articleSchema = new mongoose.Schema({
   description: {
     type: String
   },
-  details: {
-    type: String
+  markdown: {
+    type: String,
+    required: true
   },
   createdAt: {
     type: Date,
-    default: Date.now 
+    default: Date.now
   },
-  category: {
-    type: String
+  slug: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  sanitizedHtml: {
+    type: String,
+    required: true
+  }
+})
+
+articleSchema.pre('validate', function(next) {
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true })
   }
 
-});
+  if (this.markdown) {
+    this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
+  }
 
-  articleSchema.pre('validate', function (next) {
-    if (this.title) {
-      this.slug = slugify(this.title, {lower: true, strict: true})
-    }
+  next()
+})
 
-    next();
-  })
-
-module.exports = mongoose.model('Article', articleSchema)//export table article with its schema 
+module.exports = mongoose.model('Article', articleSchema)
