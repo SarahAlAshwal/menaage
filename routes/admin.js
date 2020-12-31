731f944cc9bb0f3279ccbc2b3780d7d5f77c752a
SarahAlAshwal <sa.alashwal@gmail.com>
2020-12-31 15:41:02 -0500

    add educational videos to the dashboard

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Article = require ('../models/article');
 const LiveInterviews = require("../models/liveInterviews");
+const EducationalVideos = require("../models/educationalVideos");
 
 router.get('/', async(req,res)=>{
   const articles = await Article.find().sort({createdAt: 'desc'}); //gives all the articles
   const liveInterviews = await LiveInterviews.find().sort({createdAt: "desc"});
+  const educationalVideos = await EducationalVideos.find().sort({createdAt: "desc",});
   // we can pass any object to index
-  res.render('admin/admin', {articles: articles, liveInterviews: liveInterviews});
+  res.render('admin/admin', {articles: articles, liveInterviews: liveInterviews, educationalVideos: educationalVideos});
 })
 
 router.get('/articles/new', (req, res) => {
@@ -58,8 +60,62 @@ router.delete("/liveInterviews/:id", async (req, res) => {
 router.put( "/liveInterviews/:id", async (req, res, next) => {
     req.liveInterview = await LiveInterviews.findById(req.params.id);
     next();
-  },
-  saveInterviewAndRedirict("/liveInterviews/edit"));
+  }, saveInterviewAndRedirict("/liveInterviews/edit"));
+
+  
+  router.get("/educationalVideos/new", (req, res) => {
+    res.render("educationalVideos/new", {
+      educationalVideo: new EducationalVideos(),
+    });
+  });
+  
+  router.get("/educationalVideos/edit/:id", async (req, res) => {
+    const educationalVideo = await EducationalVideos.findById(req.params.id);
+    res.render("educationalVideos/edit", {
+      educationalVideo: educationalVideo,
+    });
+  });
+  
+  router.post("/educationalVideos",async (req, res, next) => {
+      req.educationalVideo = new EducationalVideos();
+      next();
+    },
+    saveeducationalVideoAndRedirict("new")
+  );
+  
+  router.put("/educationalVideos/:id",async (req, res, next) => {
+      req.educationalVideo = await EducationalVideos.findById(req.params.id);
+      next();
+    },
+    saveeducationalVideoAndRedirict("edit")
+  );
+  
+  router.delete("/educationalVideos/:id", async (req, res) => {
+    await EducationalVideos.findByIdAndDelete(req.params.id);
+    res.redirect("/admin");
+  });
+  
+  function saveeducationalVideoAndRedirict(path) {
+    return async (req, res) => {
+      let educationalVideo = req.educationalVideo;
+      educationalVideo.title = req.body.title;
+      educationalVideo.description = req.body.description;
+      educationalVideo.videoUrl = req.body.videoUrl;
+  
+      try {
+        educationalVideo = await educationalVideo.save();
+        res.redirect(`/admin`);
+      } catch (e) {
+        console.log(e);
+        res.render(`admin/${path}`, {
+          educationalVideo: educationalVideo,
+        });
+      }
+    };
+  }
+
+
+ 
 
 function saveAndRedirect(path) {
   return async (req, res) => {
